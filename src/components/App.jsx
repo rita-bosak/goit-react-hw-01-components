d814223fa38289d82957983fe69e4b859bcd2ff6
Marharyta Bosak <aboimova.rita@gmail.com>
2022-04-20 13:38:48 +0300

    create components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,29 @@
-export const App = () => {
+import user from '../json/user';
+import data from '../json/data';
+import friends from '../json/friends.json';
+import Profile from './profile/Profile';
+import Statistics from './statistics/Statistics';
+import FriendList from './FriendList/FriendList';
+import transactions from '../json/transactions';
+import TransactionHistory from './TransactionHistory/TransactionHistory';
+
+export default function App() {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        textTransform: 'uppercase',
-        color: '#010101',
-      }}
-    >
-      React homework template
+    <div>
+      <Profile
+        key={user.tag}
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        followers={user.stats.followers}
+        views={user.stats.views}
+        likes={user.stats.likes}
+      />
+      <Statistics title="Upload stats" stats={data} />
+      <Statistics stats={data} />
+      <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />
     </div>
   );
-};
+}
